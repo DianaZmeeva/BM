f54c63c2780ae6b95d6b147eb9c3619bca5b4ac9
DianaZmeeva <50080556+DianaZmeeva@users.noreply.github.com>
2019-05-15 17:56:44 +0700

    Starts doing classes
    Starts doing classes

---
+++ BM_project/Assets/Scripts/Team.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Team : MonoBehaviour
+{
+    public class TeamClass //класс команды 
+    {
+        public double l; //рейтинг команды (лямбда в распределении)
+        public double attack;// коэффицент атаки
+        public double defence; //  коэффицент защиты
+        public int n; // количество матчей 
+        public int goals; //  количество пропущенных мячей
+        public int missed; // количество пропущенных мячей
+        public string name; // Название команды
+        public int money; //бюджет команды
+
+
+        public TeamClass()
+        {
+           
+        }
+    }
+
+    public class Champ //класс турнира 
+    {
+        public List<Game> games;
+        public List<TeamClass> teams;
+        public TeamClass myteam;
+
+        public Champ(TeamClass t)
+        {
+            myteam = t;
+            teams.Add(myteam);
+        }
+
+        public void GenerateTeams()
+        {
+            string[] country = {"Франция","Германия", "США", "Италия", "Испания", "Бельгия", "Польша", "Китай", "Япония" }; ;
+            for (int i=0; i < 3; i++)
+            {
+                System.Random rnd = new System.Random();
+                int f = rnd.Next();
+                TeamClass t = new TeamClass();
+                t.name = country[rnd.Next(country.Length)];
+                t.l = rnd.Next(30, 60);
+                teams.Add(t);
+            }
+        }
+    }
+
+    public class Game // класс игры
+    {
+        double x1, x2;
+        public TeamClass team1, team2; // индексы команд
+        public int o1, o2; //очки команд
+        public Game(TeamClass i, TeamClass j)
+        {
+            team1 = i;
+            team2 = j;
+        }
+
+        public void PlayGame() 
+        {
+            Dictionary<int, double> p1 = new Dictionary<int, double>(); //словарь вероятностей для команды 1 
+            Dictionary<int, double> p2 = new Dictionary<int, double>(); //словарь вероятностей для команды 1 
+
+            for (int i=0; i<100; i++)
+            {
+                x1 = Puasson(team1.l, i);
+                x2 = Puasson(team2.l, i);
+                if (x1 > 0)
+                {
+                   p1.Add(i, x1);
+                }
+                if (x2 > 0)
+                {
+                    p1.Add(i, x2);
+                }
+            }
+
+            o1 = Goal_Generate(p1);
+            o2 = Goal_Generate(p2);
+            if (o1 == 0)
+                o2 = 20;
+            if (o2 == 0)
+                o1 = 20;
+            if (o1 == o2)
+            {
+                System.Random rnd = new System.Random();
+                x1 = rnd.NextDouble();
+                if (x1 <= 0.5)
+                    o1 += 2;
+                else
+                    o2 += 2;
+            }
+        }
+
+        private int Goal_Generate(Dictionary<int, double> p1)
+        {
+            System.Random rnd = new System.Random();
+            double x1 = rnd.NextDouble();
+            int i = -1;
+            while (x1 > 0)
+            {
+                i++;
+                x1 -= p1[i];
+            }
+            return i;
+        }
+
+        public double Puasson(double l, int m)
+        {
+            double x = (Math.Pow(l, m) / F(m)) * Math.Exp(-l);
+            return x;
+        }
+
+        public int F(int z)
+        {
+            int s = 1;
+            for (int i = z; i > 1; i--)
+                s *= i;
+            return s;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
