62139bad8532626c348e38577d43fb6b377aba38
DianaZmeeva <50080556+DianaZmeeva@users.noreply.github.com>
2019-05-19 18:15:05 +0700

    Fix problem
    Fix problem with function onMouseDown()

--- BM_project/Assets/Scripts/Home.cs
+++ BM_project/Assets/Scripts/Home.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Home : MonoBehaviour
 {
-    public void OnMouseDown()
+    void OnMouseDown()
     {
         SceneManager.LoadScene("Start");
     }
