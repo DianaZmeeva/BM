7bf6421638fc9cc738e320fc39785aedca372ba0
Diana Zmeeva <zmeevadiana@mail.ru>
2020-01-12 18:21:04 +0700

    refactor: delete part of incorrect comments

--- BM_project/Assets/Scripts/SportManagerController.cs
+++ BM_project/Assets/Scripts/SportManagerController.cs
@@ -9,20 +9,20 @@ using System.Linq;
 public class SportManagerController : MonoBehaviour
 {
 
-    public class TeamClass //класс команды 
+    public class TeamClass
     {
-        public double TeamRating; //рейтинг команды (лямбда в распределении)
-        public int PlaceInChamp; // место, занятое командой
-        public int TeamNumber; // номер команды
-        public int GoalsInChamp; //  количество забитых мячей
-        public int MissedGoalsInChamp; // количество пропущенных мячей
-        public string TeamName; // Название команды
-        public int Budget=100; //бюджет команды
-        public int PointsInChamp; // количество очков в турнире
-        public int WinsInChamp; // количество побед в турнире
-        public int DefeatsInChamp; //количесвто поражений в турнире
-        public int NumberOfAllWins; //количество побед всего
-        public int NumberOfAllDefeats; //количество поражений всего
+        public double TeamRating; 
+        public int PlaceInChamp;
+        public int TeamNumber; 
+        public int GoalsInChamp; 
+        public int MissedGoalsInChamp;
+        public string TeamName; 
+        public int Budget=100; 
+        public int PointsInChamp; 
+        public int WinsInChamp; 
+        public int DefeatsInChamp; 
+        public int NumberOfAllWins;
+        public int NumberOfAllDefeats; 
 
         public TeamClass(string name, double rating, int number)
         {
@@ -44,13 +44,13 @@ public class SportManagerController : MonoBehaviour
         }
     }
 
-    public class Champ //класс турнира 
+    public class Champ
     {
-        public int NumberOfPlayedGames; //количество игр в турнире 
-        public int EarningsRatioForChamp;  //коэффицент заработка за турнир (используется при подсчете призовых денег за турнир)
-        public List<Game> GamesInChamp = new List<Game>(); // игры турнира
-        public List<TeamClass> TeamsInChamp = new List<TeamClass>(); //команды, участвующие в турнире
-        public TeamClass PlayerTeam; //комнада игрока
+        public int NumberOfPlayedGames;
+        public int EarningsRatioForChamp; 
+        public List<Game> GamesInChamp = new List<Game>(); 
+        public List<TeamClass> TeamsInChamp = new List<TeamClass>(); 
+        public TeamClass PlayerTeam; 
 
         public Champ(TeamClass team)
         {
@@ -58,25 +58,21 @@ public class SportManagerController : MonoBehaviour
             TeamsInChamp.Add(PlayerTeam);
         }
 
-        //функция создания команд-соперников на турнир (задание имени, рейтинга команды (в пределах min-max), номера команды)
         public void GenerateRivalTeams(int minRating, int maxRating)
         {
             System.Random rnd = new System.Random();
             string[] country = { "Франция", "Германия", "США", "Италия", "Испания", "Бельгия", "Польша", "Китай", "Япония" }; ;
             for (int i = 0; i < 3; i++)
             {
-                //TeamClass t = new TeamClass(country[rnd.Next(country.Length)], rnd.Next(30, 60));
-                //t.TeamName = country[rnd.Next(country.Length)];
-                //t.TeamRating = rnd.Next(30, 60);
                 TeamsInChamp.Add(new TeamClass(country[rnd.Next(country.Length)] + "_" + (i + 1), rnd.Next(minRating, maxRating), i+1));
             }
         }
     }
 
-    public class Game // класс игры
+    public class Game 
     {
-        public TeamClass FirstTeamInGame, SecondTeamInGame; // индексы команд
-        public int FirstTeamPoints, SecondTeamPoints; //очки команд
+        public TeamClass FirstTeamInGame, SecondTeamInGame;
+        public int FirstTeamPoints, SecondTeamPoints;
         private const int PointsWithTechnicalVictory = 20;
         private const int PointsWithTechnicalLoose = 0;
         public Game(TeamClass team1, TeamClass team2)
@@ -136,7 +132,6 @@ public class SportManagerController : MonoBehaviour
             return probabilityDictionary;
         }
 
-        //генератор Пуассоновского распределения
         private int GoalGenerateByPuasson(Dictionary<int, double> probabilityDictionary)
         {
             System.Random rnd = new System.Random();
@@ -151,13 +146,11 @@ public class SportManagerController : MonoBehaviour
             return value;
         }
         
-        //расчет вероятностей забития m очков командой с рейтингом TeamRating по Пуассоновскому распределению 
         public double GetProbabilityByPuassonDistribution(double rating, int numberOfPoints)
         {
             return (Math.Pow(rating, numberOfPoints) / GetFactorial(numberOfPoints)) * Math.Exp(-rating);
         }
 
-        // функция рассчета факториала
         public double GetFactorial(int value)
         {
             double factorial = 1;
@@ -167,7 +160,7 @@ public class SportManagerController : MonoBehaviour
         }
     }
 
-    TeamClass _myGameTeam; // команда игрока
+    TeamClass _myGameTeam; 
     Champ _currentChamp;
 
     //UI-элементы для турнира (CanvasChamp)
@@ -197,25 +190,15 @@ public class SportManagerController : MonoBehaviour
     public Text warning;  //текстовое поле - ошибка (отсутвие выбранного уровня сложности)
 
 
-    // Start is called before the first frame update
     void Start()
     {
         System.Random rnd = new System.Random();
         Change_canvas(true, false, false, false);
        
 
-        //создание команды игрока
         _myGameTeam = new TeamClass("Россия", rnd.Next(29,35), 0);
         Write_about_team();
 
-        //Champ currentChamp = new Champ(myGameTeam);
-        //currentChamp.GenerateRivalTeams();
-        //Game g = new Game(currentChamp.TeamsInChamp[2], currentChamp.TeamsInChamp[3]);
-        //g.PlayGame();
-        //currentChamp.GamesInChamp.Add(g);
-        //DontDestroyOnLoad(this.gameObject);
-
-        //обработчики нажатия кнопок
         Button start = playChamp.GetComponent<Button>();
         start.onClick.AddListener(StartChamp);
         back_to_team.onClick.AddListener(BackToGameTeam);
@@ -230,7 +213,7 @@ public class SportManagerController : MonoBehaviour
         ListOfButtonGame[5].onClick.AddListener(() => play(2, 3, 5, 11, 8));
     }
 
-    //функция прокачки команды  (i - служебная переменная, для определения места выхова функции)
+    //i - служебная переменная, для определения места выхова функции
     private void Up_team(int i)
     {
 
@@ -288,7 +271,6 @@ public class SportManagerController : MonoBehaviour
         }
     }
 
-    // функция вывода статистики команд в турнире
     private void Statistic()
     {
         Change_canvas(false, false, false, true);
@@ -304,23 +286,23 @@ public class SportManagerController : MonoBehaviour
         }
     }
 
-    //функция возвращения обратно к таблице турнира
+   
     private void Back_to_champ()
     {
-        Change_canvas(false, true, true, false); //изменения отображения Canvas
+        Change_canvas(false, true, true, false); 
     }
 
-    //функция возвращения обратно к команде игрока
+
     void BackToGameTeam()
     {
         Up_team(0); //вызов функции (со значение 0) - для вывода стоимости, необходимой для прокачки команды
-        Change_my_team(); //изменения значений комнады после турнира
-        Write_about_team(); //изменение UI-элементов о команде
-        Clear(); // очищение таблицы-турнира
-        Change_canvas(true, false, false, false);//изменения отображения Canvas
+        Change_my_team(); 
+        Write_about_team(); 
+        Clear(); 
+        Change_canvas(true, false, false, false);
     }
 
-    //функция старта турнира
+   
     void StartChamp()
     {
         _currentChamp = new Champ(_myGameTeam);
@@ -352,27 +334,26 @@ public class SportManagerController : MonoBehaviour
                 _currentChamp.EarningsRatioForChamp = 100;
             }
 
-            Change_canvas(false, true, false, false);//открытие таблицы-турнира
-            _currentChamp.GenerateRivalTeams(minRating, maxRating); //генерация команд соперников
+            Change_canvas(false, true, false, false);
+            _currentChamp.GenerateRivalTeams(minRating, maxRating); 
 
             for (int i = 0; i < _currentChamp.TeamsInChamp.Count; i++) 
             {
-                team_text[i].text = _currentChamp.TeamsInChamp[i].TeamName;//вывод списка комнад участников турнира
+                team_text[i].text = _currentChamp.TeamsInChamp[i].TeamName;
             }
         }
-        else //если уровень не выбран, показать сообщение об ошибке
+        else 
         {
             warning.text = "Choose level!";
         }
 
     }
 
-    //функция игры между 2 комадами
+    
     void play(int numberOfFirstTeam, int numberOfSecondTeam, int numberOfButtonPlay, int numberOfFirstTeamPointsText, int numberOfSecondTeamPointsText)// i,j-номер команд; b-номер кнопки в массиве; f,s - номера тектовых полей для вывода счета
     {
         ListOfButtonGame[numberOfButtonPlay].enabled = false;
 
-        //выяснение результатов игры 
         Game newGame = new Game(_currentChamp.TeamsInChamp[numberOfFirstTeam], _currentChamp.TeamsInChamp[numberOfSecondTeam]);
         newGame.PlayGame();
         _currentChamp.GamesInChamp.Add(newGame);
@@ -392,24 +373,21 @@ public class SportManagerController : MonoBehaviour
         if (_currentChamp.NumberOfPlayedGames == 6) //проверка: является ли игра последней в турнире
         {
             var sortedTeams = _currentChamp.TeamsInChamp.OrderByDescending(u => u.PointsInChamp)
-                .ThenByDescending(u => (u.GoalsInChamp - u.MissedGoalsInChamp)); //сортировка команд по местам (по количесвту очков; при равенстве таковых по разнице забитых-пропущенных)
+                .ThenByDescending(u => (u.GoalsInChamp - u.MissedGoalsInChamp)); 
 
             int teamPlace = 1;
-            foreach (TeamClass team in sortedTeams) //отображение результатов турнира
+            foreach (TeamClass team in sortedTeams)
             {
                 _currentChamp.TeamsInChamp[team.TeamNumber].PlaceInChamp = teamPlace;
                 place_text[team.TeamNumber].text = teamPlace.ToString();
                 teamPlace++;
             }
-            Change_canvas(false, true, true,false);//отображение PanelMenu
+            Change_canvas(false, true, true,false);
         }
     }
 
-    //функция очистки турнира
     private void Clear()
     {
-        //очистка текстовых поле таблици-турнира
-
         for (int i=0; i<ListOfButtonGame.Count; i++)
         {
             ListOfButtonGame[i].enabled = true;
@@ -427,7 +405,6 @@ public class SportManagerController : MonoBehaviour
             place_text[i].text = "";
         }
 
-        //очистка турнирных показателей команды игрока
         _myGameTeam.GoalsInChamp = 0;
         _myGameTeam.MissedGoalsInChamp = 0;
         _myGameTeam.PointsInChamp = 0;
@@ -436,24 +413,21 @@ public class SportManagerController : MonoBehaviour
         _myGameTeam.DefeatsInChamp = 0;
     }
 
-    //изменение отображения Canvas
     void Change_canvas(bool isFirstCanvasActive, bool isSecondCanvasActive, bool isThirdCanvasActive, bool isFourthCanvasActive)
     {
-        //GameObject t1 = GameObject.Find("Canvas_team") as GameObject;
         t1.SetActive(isFirstCanvasActive);
         t2.SetActive(isSecondCanvasActive);
         t3.SetActive(isThirdCanvasActive);
         t4.SetActive(isFourthCanvasActive);
     }
 
-    //функция изменения показателей команды игрока после турнира 
     private void Change_my_team()
     {
-        //измененеие количества побед/поражений
+        
         _myGameTeam.NumberOfAllWins += _myGameTeam.WinsInChamp;
         _myGameTeam.NumberOfAllDefeats += _myGameTeam.DefeatsInChamp;
 
-        //измененение бюджета команды игрока при занятии призового места
+        
         if (_myGameTeam.PlaceInChamp==3)
             _myGameTeam.Budget += 10 * _currentChamp.EarningsRatioForChamp;
         if (_myGameTeam.PlaceInChamp == 2)
@@ -463,7 +437,7 @@ public class SportManagerController : MonoBehaviour
 
     }
 
-    //измененение тектовой информации о команде игрока  (на экране CanvasTeam)
+    
     public void Write_about_team()
     {
         r.text= Math.Round(((_myGameTeam.TeamRating * 100) / 73), 1) + "%"; //подсчет рейтинга команды в процентах
